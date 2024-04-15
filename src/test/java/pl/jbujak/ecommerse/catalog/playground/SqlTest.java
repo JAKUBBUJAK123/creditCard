56f00be2a8e1298c557d8cf6f57e5a2c7838fc43
s-301-27 <s-301-27@gumiforest.uek.krakow.pl>
2024-04-15 16:30:19 +0200

    creating database server

---
+++ src/test/java/pl/jbujak/ecommerse/catalog/playground/SqlTest.java
@@ -0,0 +1,23 @@
+package pl.jbujak.ecommerse.catalog.playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import  static org.assertj.core.api.Assertions.*;
+@SpringBootTest
+public class SqlTest {
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+    @Test
+    void itSelectForCurrentState() {
+        var myDate = jdbcTemplate.queryForObject("Select now() myCurrentDate"
+        ,String.class);
+        assertThat(myDate).contains("2024");
+    };
+    @Test
+    void iAmAbleToCreateTable() {
+        var sql = "Create table xyz ()";
+    }
+
+}
